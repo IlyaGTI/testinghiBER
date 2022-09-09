5604977ec981bfbe3935a0de7f1920890b4b9715
vipku <vip.kufaev@mail.ru>
2022-09-09 21:42:35 +0500

    Adding Model, Repo, Service x2

--- src/main/java/com/example/testing/controller/EmployeeController.java
+++ src/main/java/com/example/testing/controller/EmployeeController.java
@@ -1,7 +1,35 @@
 package com.example.testing.controller;
 
-import javax.ws.rs.Path;
+import com.example.testing.model.Employee;
+import com.example.testing.service.EmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@RestController
+@RequestMapping("/empl")
+@Api
 @Path("/empl")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping(path = "/cr", produces = MediaType.APPLICATION_JSON)
+    @ApiOperation("создание работника")
+    @POST()
+    public Employee createEmpl(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
 }
